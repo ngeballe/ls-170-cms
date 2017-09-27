67fa5fb05ca5ec2f33066584958e2ecf0f5cdfb4
Nick Geballe <ngeballe@gmail.com>
2017-09-27 13:02:08 -0400

    Have index page link to files, clicking on link leads to file displayed as plaintext

--- cms.rb
+++ cms.rb
@@ -2,11 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
-get "/" do
-  root = File.expand_path("..", __FILE__)
+before do
+  @root = File.expand_path("..", __FILE__)
+end
 
-  @files = Dir.glob(root + "/data/*").map do |path|
+get "/" do
+  @files = Dir.glob(@root + "/data/*").map do |path|
     File.basename(path)
   end
+
   erb :index
+end
+
+get "/:file" do
+  file_path = "#{@root}/data/#{params[:file]}"
+
+  headers["Content-Type"] = "text/plain"
+
+  File.read(file_path)
 end
